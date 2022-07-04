a4cebb61cb3a51bb265998e3b146cf3c36854b33
Kelvin Parmar <94775043+kelvinparmar@users.noreply.github.com>
2022-07-04 22:31:54 +0530

    Add files via upload

---
+++ tinder-clone/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lets builld tinder clone By MERN stack</h1>
+      
+      <header />
+      
+      
+      {/*header*/}
+      {/*TinderCards*/}
+      {/*Buttons*/}
+    </div>
+  );
+}
+
+export default App;
